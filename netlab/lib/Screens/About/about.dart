96e44caa562b678754f9bc297286c2a80efa2e66
Francois Lannoy <flannoy154@gmail.com>
2020-12-02 09:58:02 +0100

    :sparkles: Home and about functionnal

--- netlab/lib/Screens/About/about.dart
+++ netlab/lib/Screens/About/about.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
 
-class About extends StatefulWidget {
-  About({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _AboutState createState() => _AboutState();
-}
-
-class _AboutState extends State<About> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
