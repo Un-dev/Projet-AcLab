cd9efa8c21fe3c8c6c9965b8c19ed6a5f8050bb0
Francois Lannoy <flannoy154@gmail.com>
2020-12-02 14:28:33 +0100

    no need for a createroom widget

--- netlab/lib/Screens/Room/Room.dart
+++ netlab/lib/Screens/Room/Room.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class CreateRoom extends StatelessWidget {
+class Room extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('CreateRoom')),
+      appBar: AppBar(title: Text('Room')),
     );
   }
 }
