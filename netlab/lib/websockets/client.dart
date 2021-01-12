a4ab56a0713801f6438d803ac870940973733037
Galimede <mat.degand@outlook.fr>
2021-01-12 09:23:13 +0100

    :bug: Fix a bug where client wouldnt receive req info

--- netlab/lib/websockets/client.dart
+++ netlab/lib/websockets/client.dart
@@ -10,8 +10,15 @@ void main() {
   socket.onConnect((_) {
     print(socket.id);
     socket.emit('create_room', {'username': 'toto'});
+    socket.emit('show-members');
   });
+
+  _showMembers(dynamic data) {
+    print(data);
+  }
+
   socket.on('event', (data) => print(data));
+  socket.on('show-members', (data) => _showMembers(data));
   socket.onDisconnect((_) => print('disconnect'));
   socket.on('fromServer', (_) => print(_));
 }
