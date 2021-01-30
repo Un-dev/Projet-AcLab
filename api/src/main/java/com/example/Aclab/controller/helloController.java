b7cc1741e6e764c4ead561c5667042551ce282f4
verzelea <arthurverzele@hotmail.com>
2021-01-30 14:09:31 +0100

    :adhesive_bandage: clean code

--- api/src/main/java/com/example/Aclab/controller/helloController.java
+++ api/src/main/java/com/example/Aclab/controller/helloController.java
@@ -5,45 +5,16 @@ import com.uwetrottmann.tmdb2.Tmdb;
 import com.uwetrottmann.tmdb2.entities.Movie;
 import com.uwetrottmann.tmdb2.entities.MovieResultsPage;
 import com.uwetrottmann.tmdb2.services.MoviesService;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import retrofit2.Response;
 
-import java.io.*;
-import java.net.URL;
-import java.nio.charset.Charset;
-
 @RestController
 @RequestMapping("api")
 public class helloController {
-
-
-    /*@GetMapping
-    public Movie hello(){
-        Tmdb tmdb = new Tmdb(AclabApplication.API_KEY);
-        MoviesService moviesService = tmdb.moviesService();
-        Movie movieRes = null;
-        // Call any of the available endpoints
-        try {
-            Response<Movie> response = moviesService.summary(550, "fr")
-                    .execute();
-            if (response.isSuccessful()) {
-                Movie movie = response.body();
-                movieRes = movie;
-                System.out.println(movie.title + " is awesome!");
-            }
-        } catch (Exception e) {
-            System.out.println("error");
-        }
-        return movieRes;
-    }*/
 
     @GetMapping
     public MovieResultsPage GetAllMovieByPopularity(@RequestHeader("page") int page){
@@ -82,5 +53,4 @@ public class helloController {
         }
         return movieRes;
     }
-
 }
